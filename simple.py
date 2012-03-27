34c69822e18399c65b41935969e2760556ca4ed0
Orf <tomforbes8@hotmail.com>
2012-03-28 00:06:23 +0100

    Removed models.py and moved the model into simple.py.

--- simple.py
+++ simple.py
@@ -7,12 +7,26 @@ from unicodedata import normalize
 from flaskext.sqlalchemy import SQLAlchemy
 import datetime
 from unicodedata import normalize
+import markdown
 
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id    = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=True)
+    slug  = db.Column(db.String(), unique=True)
+    text  = db.Column(db.String(), default="")
+    draft = db.Column(db.Boolean(), index=True, default=True)
+    created_at = db.Column(db.DateTime, index=True)
+    updated_at = db.Column(db.DateTime)
+
+    def render_content(self):
+        return markdown.Markdown(extensions=['fenced_code'], output_format="html5", safe_mode=True).convert(self.text)
 
 def requires_authentication(f):
     @wraps(f)
