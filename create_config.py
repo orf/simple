67f5df2b075ea60db4191bbccef6a50363904a5c
Orf <tomforbes8@hotmail.com>
2012-03-28 00:11:47 +0100

    Fixed some settings related stuff

--- create_config.py
+++ create_config.py
@@ -1,4 +1,4 @@
-import os
+import hashlib
 
 def input_with_default(prompt, default):
     x = raw_input("%s (Default %s) "%(prompt, default))
@@ -8,10 +8,9 @@ def input_with_default(prompt, default):
 
 with open("settings.py", "w") as fd:
     print "Generating a Simple config file. Please answer some questions:"
-    fd.write('SECRET_KEY = r"%s"\n'%os.urandom(128))
     fd.write("POSTS_PER_PAGE = %s\n"%input_with_default("Posts per page", 5))
     fd.write("ADMIN_USERNAME = '%s'\n"%input_with_default("Admin username","admin"))
-    fd.write("ADMIN_PASSWORD = '%s'\n"%input_with_default("Admin password","password"))
+    fd.write("ADMIN_PASSWORD = '%s'\n"%hashlib.md5(input_with_default("Admin password","password")).hexdigest())
     fd.write("ANALYTICS_ID = '%s'\n"%input_with_default("Google analytics ID",""))
     fd.write('SQLALCHEMY_DATABASE_URI = "%s"'%input_with_default("Database URI","sqlite:///simple.db"))
     fd.write("GITHUB_USERNAME = '%s'"%input_with_default("Github Username", ""))
