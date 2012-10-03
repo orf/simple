d3c7705c6a39102b4eefadb824744551864819af
Tom <tom@tomforb.es>
2012-10-03 15:31:15 +0100

    Added a SHOW_VIEWS_ON_HOMEPAGE setting which does what it says on the tin.

--- create_config.py
+++ create_config.py
@@ -12,6 +12,7 @@ print "Generating a Simple config file. Please answer some questions:"
 SETTINGS = (
     input_with_default("Posts per page", 5),
     input_with_default("Show the post content on the homepage","y").lower()[0] == "y",
+    input_with_default("Show post view count on the homepage?","n").lower()[0] == "y",
     input_with_default("Admin username","admin"),
     generate_password_hash(input_with_default("Admin password","password")), 
     input_with_default("Google analytics ID",""),
@@ -28,6 +29,7 @@ with open("settings.py", "w") as fd:
     fd.write("""# -*- coding: utf-8 -*-\n
 POSTS_PER_PAGE = %s
 POST_CONTENT_ON_HOMEPAGE = %s
+SHOW_VIEWS_ON_HOMEPAGE = %s
 ADMIN_USERNAME = '%s'
 ADMIN_PASSWORD = '%s'
 ANALYTICS_ID = '%s'
