be193874d4178e729dc0b16bd22824d18095ca05
orf <tom@tomforb.es>
2013-02-16 06:39:48 +0000

    Has to be .String(), .LargeBinary() can't be filtered on it seems

--- simple.py
+++ simple.py
@@ -46,7 +46,7 @@ class Post(db.Model):
     __tablename__ = "posts"
     id    = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
-    slug  = db.Column(db.LargeBinary(), unique=True)
+    slug  = db.Column(db.String(), unique=True)
     text  = db.Column(db.String(), default="")
     draft = db.Column(db.Boolean(), index=True, default=True)
     views = db.Column(db.Integer(), default=0)
