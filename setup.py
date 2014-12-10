6fb16fa62d60a1a7be98d4bf396dfd359e8df8fe
orf <tom@tomforb.es>
2014-12-10 22:56:23 +0000

    Add Manifest.in

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Orf',
     author_email='tom@tomforb.es',
     description='',
+    include_package_data=True,
     install_requires=["flask", "flask_seasurf", "pony", "markdown", "python_dateutil",
               "flask_basicauth", 'requests', 'flask_script'],
     entry_points={
