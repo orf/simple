bf6bf3b7c95b7f6ba6eae8659b3b21b429a853ce
orf <tom@tomforb.es>
2015-01-25 18:55:33 +0000

    Bump version and fix nginx_config from not working

--- simple/commands.py
+++ simple/commands.py
@@ -112,7 +112,8 @@ def create():
 
 
 @manager.command
-def nginx_config(domain_name, proxy_port="9000", use_pagespeed=False):
+@manager.option("-p", "--proxy-port", dest="proxy_port", default="9000")
+def nginx_config(domain_name, use_pagespeed=False):
     """ Create a nginx config file and output it to stdout """
 
     if app == DEFAULT_APP:
