e657fb134f093542cce6d2a2bce8e7bb062b49c4
orf <tom@tomforb.es>
2015-09-30 15:40:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleblogging',
-    version='2.0.6',
+    version='2.0.7',
     packages=['simple'],
     url='http://github.com/orf/simple',
     license='MIT',
