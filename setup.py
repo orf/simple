e36dd1a9ae2e1f509d6f6a17fdc5161f4ba0b950
Tom <tom@tomforb.es>
2016-08-21 13:02:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleblogging',
-    version='2.0.12',
+    version='2.0.13',
     packages=['simple'],
     url='http://github.com/orf/simple',
     license='MIT',
