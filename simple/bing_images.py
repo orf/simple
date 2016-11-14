0469ba1f4f1d907013c328bc2834905dd391dba8
Arie Kurniawan <Lidilidian@users.noreply.github.com>
2016-11-15 01:20:47 +0700

    Change url protocol for more secure access

--- simple/bing_images.py
+++ simple/bing_images.py
@@ -2,14 +2,14 @@ import requests
 
 
 def get_latest_header_images(idx=0, num=5):
-    resp = requests.get("http://www.bing.com/HPImageArchive.aspx?format=js&n={0}&idx={1}".format(num, idx)).json()
+    resp = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&n={0}&idx={1}".format(num, idx)).json()
     
     if resp is None:
         return {}
 
     return {
         "images": [
-            {"url": "http://bing.com" + item["url"], "copyright": item["copyright"]} for item in resp["images"]
+            {"url": "https://bing.com" + item["url"], "copyright": item["copyright"]} for item in resp["images"]
         ]
     }
 
